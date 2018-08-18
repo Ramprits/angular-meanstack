5a19a480bea8fe25295d1ed7cf1ed69bcfcbdd29
ramprit <Rampritsahani@outlook.in>
2018-08-18 16:58:18 +0530

    init update

---
+++ apps/web-blog/src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+// xplat
+import { ScopeCoreModule } from '@myworkspace/web';
+
+@NgModule({
+  imports: [ScopeCoreModule]
+})
+export class CoreModule {}
